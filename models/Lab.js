d9c9e56c0ae276c9479847f8ae28273bf988a44f
clinicprosystem <clinicprosystem@gmail.com>
2026-06-06 14:49:33 +0300

    Update Lab.js

--- models/Lab.js
+++ models/Lab.js
@@ -4,8 +4,8 @@ const labSchema = new mongoose.Schema({
     clinicId: { type: mongoose.Schema.Types.ObjectId, ref: 'Clinic', required: true },
     name: { type: String, required: true },
     phone: { type: String, required: true },
-    address: String,
-    notes: String,
+    address: { type: String, default: '' },
+    notes: { type: String, default: '' },
     createdAt: { type: Date, default: Date.now }
 });
 
